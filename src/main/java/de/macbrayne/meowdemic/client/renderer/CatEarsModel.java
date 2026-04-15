714ea17aa0e088b867fa6b2c1a21f2b219b712de
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2026-04-15 19:11:09 +0200

    Second attempt at a cat ears model

--- src/main/java/de/macbrayne/meowdemic/client/renderer/CatEarsModel.java
+++ src/main/java/de/macbrayne/meowdemic/client/renderer/CatEarsModel.java
@@ -19,12 +19,14 @@ public class CatEarsModel extends PlayerModel {
         PartDefinition root = mesh.getRoot().clearRecursively();
         PartDefinition head = root.getChild("head");
 
-        PartDefinition catEars = head.addOrReplaceChild("cat_ears", CubeListBuilder.create().texOffs(0, 0).addBox(2.0F, -7.0F, -8.0F, 1.0F, 1.0F, 0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-4.0F, -6.0F, -8.0F, 1.0F, 2.0F, 0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-2.0F, -6.0F, -8.0F, 1.0F, 2.0F, 0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(3.0F, -6.0F, -8.0F, 1.0F, 2.0F, 0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(1.0F, -6.0F, -8.0F, 1.0F, 2.0F, 0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-3.0F, -7.0F, -8.0F, 1.0F, 1.0F, 0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -4.0F, 4.0F));
+        PartDefinition catEars = head.addOrReplaceChild("cat_ears", CubeListBuilder.create().texOffs(0, 0).addBox(2.0F, -7.0F, -8.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-4.0F, -6.0F, -8.0F, 1.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-2.0F, -6.0F, -8.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(3.0F, -6.0F, -8.0F, 1.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(1.0F, -6.0F, -8.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-3.0F, -7.0F, -8.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(0.0F, -5.0F, -8.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-1.0F, -5.0F, -8.0F, 1.0F, 1.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -4.0F, 4.0F));
 
         return LayerDefinition.create(mesh, 16, 16);
     }
