83faeb865fcbb69c01989af5fbbd367b24c851be
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2026-04-16 13:44:08 +0200

    Add additional chat variants

--- src/main/java/de/macbrayne/meowdemic/events/MessageEvents.java
+++ src/main/java/de/macbrayne/meowdemic/events/MessageEvents.java
@@ -3,20 +3,31 @@ package de.macbrayne.meowdemic.events;
 
 import de.macbrayne.meowdemic.attachments.entity.TransmissionComponent;
 import de.macbrayne.meowdemic.data.Symptoms;
-import de.macbrayne.meowdemic.data.TransmissionEvent;
 import eu.pb4.placeholders.api.PlaceholderContext;
 import eu.pb4.placeholders.api.node.TextNode;
 import net.minecraft.world.entity.LivingEntity;
 
-import java.util.Optional;
-
 public class MessageEvents {
     public static TextNode register(TextNode textNode, PlaceholderContext placeholderContext) {
-        if(placeholderContext.entity() instanceof LivingEntity entity) {
-            if(TransmissionComponent.get(entity).hasSymptom(Symptoms.CHAT)) {
-                return TextNode.asSingle(textNode, TextNode.of(" meow :3"));
+        if (placeholderContext.entity() instanceof LivingEntity entity) {
+            if (TransmissionComponent.get(entity).hasSymptom(Symptoms.CHAT)) {
+                return TextNode.asSingle(textNode, TextNode.of(getVariant(entity)));
             }
         }
         return textNode;
+    }
+
+    private static String getVariant(LivingEntity entity) {
+        int random = entity.getRandom().nextInt(4);
+        String append = switch (random) {
+            case 1 -> "mew";
+            case 2 -> "nya";
+            case 3 -> "meowww";
+            default -> "meow";
+        };
+        if (entity.getRandom().nextBoolean()) {
+            append += " :3";
+        }
+        return append;
     }
 }
